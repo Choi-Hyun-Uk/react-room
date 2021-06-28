e950e9b77e3263a40e20d935604e64cdfbb8742e
chu <akdlgusdnr26@gmail.com>
2021-06-28 16:29:30 +0900

    create  MainPageWrapper styled

--- styles/index.js
+++ styles/index.js
@@ -9,6 +9,42 @@ export const RoomsListWrapper = styled.div`
         display: flex;
         flex-direction: row;
     }
+
+    & .registerpage-link {
+        position: fixed;
+        display: flex;
+        left: 50%;
+        transform: translateX(-50%);
+        bottom: 3rem;
+        & a {
+            display: block;
+            padding: 0.75rem 1.5rem;
+            background-color: #4c6ef5;
+            color: white;
+            border-radius: 0.25rem;
+            margin: 0 1rem;
+        }
+    }
+`
+
+export const MainPageWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100vh;
+    & a {
+        display: inline-block;
+        padding: 1rem 1.5rem;
+        margin: 1.5rem;
+        border-radius: 0.25rem;
+        background-color: #4c6ef5;
+        color: #fff;
+        &:hover {
+            background-color: #3b5bdb;
+        }
+    }
 `
 
 export const RegisterWrapper = styled.div`
