bb5395ab4f9abff58235543807a2e5d5c3c327d4
chu <akdlgusdnr26@gmail.com>
2021-06-28 16:25:52 +0900

    update: 수정하기 기능 추가

--- pages/room/register.jsx
+++ pages/room/register.jsx
@@ -2,22 +2,27 @@ import React from 'react';
 import RegisterForm from '../../components/register/registerForm';
 import { RegisterWrapper } from '../../styles';
 import wrapper from '../../store/configureStore';
-import { getSaveData } from '../../actions/register';
+import { getSaveData, dataGetItem } from '../../actions/register';
 
-const RoomRegisterPage = ({ isSaveData }) => {
+const RoomRegisterPage = ({ isSaveData, isUpdate }) => {
     return (
         <RegisterWrapper>
             <h1>방 등록</h1>
-            <RegisterForm isSaveData={isSaveData} />
+            <RegisterForm isSaveData={isSaveData} isUpdate={isUpdate} />
         </RegisterWrapper>
     )
 }
 
-export const getServerSideProps = wrapper.getServerSideProps((store) => async () => {
+export const getServerSideProps = wrapper.getServerSideProps((store) => async ({ ...etc }) => {
+    if (etc.query) {
+        const { id } = etc.query;
+        await store.dispatch(dataGetItem(id));
+    }
     const isSaveData = await store.dispatch(getSaveData());
     return {
         props: {
             isSaveData: Object.keys(isSaveData.payload).length === 0 ? false : true,
+            isUpdate: etc.query.id ? true : false,
         }
     }
 });
